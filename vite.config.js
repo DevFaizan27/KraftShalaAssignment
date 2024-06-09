e6e180a3174d21568d69c6cb358cddd79c86ac56
Mohd faizan Hussain <faizanhussain78612@gmail.com>
2024-06-09 10:47:44 +0530

    s

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   define: {
+//     "process.env": process.env,
+//     VITE_RAPIDAPI: process.env.VITE_RAPIDAPI,
+//   },
+// })
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   define: {
-    "process.env": process.env,
-    VITE_RAPIDAPI: process.env.VITE_RAPIDAPI,
+    'process.env': {},
+    'process.env.VITE_RAPIDAPI': JSON.stringify(process.env.VITE_RAPIDAPI),
   },
-})
+});
+
